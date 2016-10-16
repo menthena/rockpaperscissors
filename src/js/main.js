b525dd6fcccc5a17542c780d7e8e1ebd3d34fdfc
Ahmet Atasoy <menthena@gmail.com>
2016-10-16 14:37:40 +0100

    Feat(app): Adding basic navigation and interaction

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,36 @@
 require('../css/main.scss');
 
-import print from './module';
 import RockPaperScissors from './RockPaperScissors';
 
-print('it works well!');
+const gameCustomizationElement = document.getElementById('game-customization');
+const gameElement = document.getElementById('game');
+const resetButtonElement = document.getElementById('reset-game');
 
-let game = new RockPaperScissors();
-console.log(game);
+function displayGameCustomization() {
+  gameCustomizationElement.setAttribute('aria-hidden', false);
+  gameElement.setAttribute('aria-hidden', true);
+}
+
+function displayGame() {
+  gameCustomizationElement.setAttribute('aria-hidden', true);
+  gameElement.setAttribute('aria-hidden', false);
+  resetButtonElement.setAttribute('aria-hidden', false);
+}
+
+window.resetGame = () => {
+  console.log('Reset...');
+};
+
+window.startGame = () => {
+  displayGame();
+
+  const isBazingaMode = document.getElementById('game-mode-bazinga').checked;
+  const isSimulation = document.getElementById('simulate-cpu').checked;
+  console.log(isBazingaMode, isSimulation);
+  let game = new RockPaperScissors();
+  console.log(game);
+};
+
+window.newGame = () => {
+  displayGameCustomization();
+};
