b0cdbfe006f5b8f3dd3a8ab8c880a9885b7a6b97
Ahmet Atasoy <menthena@gmail.com>
2016-10-16 14:18:53 +0100

    Fix(webpack): Adding image loader

--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,13 @@ module.exports = {
 			test: /\.js$/,
 			exclude: [/node_modules/],
 			loader: 'babel'
-		}]
+		},{
+	    test: /\.(jpe?g|png|gif|svg)$/i,
+	    loaders: [
+	      'file?hash=sha512&digest=hex&name=[hash].[ext]',
+	      'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+	    ]
+	  }]
 	},
 	eslint: {
 		failOnError: true
